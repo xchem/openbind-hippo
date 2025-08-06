58041136f25a3b0e11c607efb44f8c9830063cd7
Max Winokan <mwinokan@me.com>
2025-08-06 10:19:27 +0000

    scripts

--- scripts/run_knitwork_fragment.sh
+++ scripts/run_knitwork_fragment.sh
@@ -2,21 +2,12 @@
 
 set -e
 
-KEY=$1
-
-if [[ -z $KEY ]] ; then
-	echo "no merging hypothesis nickname provided"
+if [[ -z $KNIT ]]; then
+	echo '$KNIT' variable not set
 	exit 1
 fi
 
-if [[ -z $KNIT ]]; then
-	echo '$KNIT' variable not set
-	exit 2
-fi
-
 ls $KNIT/Fragment/runEnumeration.py
-
-cd $KEY
 
 python $KNIT/Fragment/runEnumeration.py \
 	-i *_input.csv \
