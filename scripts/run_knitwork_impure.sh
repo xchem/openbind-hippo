008279411fb0ed65f4aad2633a3f173869b187d6
Max Winokan <mwinokan@me.com>
2025-08-06 10:39:13 +0000

    script

--- scripts/run_knitwork_impure.sh
+++ scripts/run_knitwork_impure.sh
@@ -2,18 +2,11 @@
 
 set -e
 
-KEY=$1
-TARGET=$2
-DIR=`dirname $(pwd)`
+TARGET=$1
 
-if [[ -z $KEY ]] ; then
-	echo "no merging hypothesis nickname provided"
+if [[ -z $TARGET ]]; then
+	echo must pass target name
 	exit 1
-fi
-
-if [[ -z $KNIT ]]; then
-	echo '$KNIT' variable not set
-	exit 2
 fi
 
 if [[ -z $KNIT ]]; then
@@ -23,18 +16,16 @@ fi
 
 ls $KNIT/Knitwork/runKnitting.py
 
-cd $KEY
-
 python $KNIT/Knitwork/runKnitting.py \
 	--substructure_pair_file fragment_output/substructure_pairs.json \
 	--descriptor prop_pharmfp \
 	--working_dir knitwork_temp \
 	--output_dir knitwork_impure_output \
 	--run_parallel \
-	--n_parallel 20 \
-	--limit 100 \
+	--n_parallel 10 \
+	--limit 50 \
 	--prolif_prioritization \
 	--max_prioritize 100 \
-	--target $(basename $DIR) \
+	--target $TARGET \
 	--substructure_dir fragment_output \
-	-fd $(dirname $DIR)
+	-fd $TARGET
