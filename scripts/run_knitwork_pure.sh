15fc6fe325bf3fc576a536939fa72c34e0046516
Max Winokan <mwinokan@me.com>
2025-08-06 10:34:49 +0000

    script

--- scripts/run_knitwork_pure.sh
+++ scripts/run_knitwork_pure.sh
@@ -2,21 +2,12 @@
 
 set -e
 
-KEY=$1
-
-if [[ -z $KEY ]] ; then
-	echo "no merging hypothesis nickname provided"
-	exit 1
-fi
-
 if [[ -z $KNIT ]]; then
 	echo '$KNIT' variable not set
 	exit 2
 fi
 
 ls $KNIT/Knitwork/runKnitting.py
-
-cd $KEY
 
 python $KNIT/Knitwork/runKnitting.py \
 	--substructure_pair_file fragment_output/substructure_pairs.json \
@@ -25,5 +16,5 @@ python $KNIT/Knitwork/runKnitting.py \
 	--output_dir knitwork_pure_output \
 	--run_parallel \
 	--n_parallel 10 \
-	--limit 100 \
+	--limit 50 \
 	--clear_working_dir
