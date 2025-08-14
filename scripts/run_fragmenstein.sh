f26948dfa76b8478a0270d55cf27842c0bcf40a7
Max Winokan <mwinokan@me.com>
2025-08-14 10:44:25 +0100

    fragmenstein

--- scripts/run_fragmenstein.sh
+++ scripts/run_fragmenstein.sh
@@ -4,10 +4,10 @@ set -e
 
 which fragmenstein
 
-time fragmenstein laboratory combine -i ../cycle_??_hits.sdf -t *_apo-desolv.pdb --victor Wictor
-
-cd ..
+#time fragmenstein laboratory combine -i ../cycle_??_hits.sdf -t *_apo-desolv.pdb --victor Wictor
+time fragmenstein laboratory combine -i ../cycle_??_hits.sdf -t *_apo-desolv.pdb --victor WictorNoPlace
 
 python ../../../scripts/fragmenstein_to_bulkdock.py
+
 
 # sb.sh --job-name "xx01zvns2b" $HOME2/slurm/run_bash_with_conda.sh run_fragmenstein.sh
