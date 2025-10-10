796edd3f63755ca0ceb53064363e6d46276f8b0d
Max Winokan <mwinokan@me.com>
2025-10-10 12:33:04 +0100

    new elabs

--- a71ev2a/cycle_01/syndirella/elabs/run_elaboration.sh
+++ a71ev2a/cycle_01/syndirella/elabs/run_elaboration.sh
@@ -4,27 +4,25 @@ set -e
 
 KEY=$1
 
-echo $MANIFOLD_API_KEY
-
-echo --input $(pwd)/$KEY".csv"
-echo --hits_path $(pwd)/"c01_elabs_syndirella_inspiration_hits.sdf"
-echo --output $(pwd)/$KEY
-echo --metadata "/opt/xchem-fragalysis-2/maxwin/BulkDock/TARGETS/A71EV2A/metadata.csv"
-echo --templates "$(pwd)/templates"
-echo --db_search_tool hippo
-echo --reference_db "/opt/xchem-fragalysis-2/maxwin/EnamineCatalogs/enamine_bb_hippo.sqlite"
+echo "SYNDIRELLA"
+echo "input        "$(pwd)/$KEY"_syndirella_input.csv"
+echo "hits_path    "$(pwd)/"a71ev2a_c01_elabs_syndirella_inspiration_hits.sdf"
+echo "output       "$(pwd)/$KEY
+echo "templates    ""$(pwd)/templates"
+echo "reference_db ""/opt/xchem-fragalysis-2/maxwin/EnamineCatalogs/enamine_bb_hippo.sqlite"
+echo
 
 /opt/xchem-fragalysis-2/maxwin/conda/bin/syndirella run \
-	--input $(pwd)/$KEY".csv" \
-	--hits_path $(pwd)/"c01_elabs_syndirella_inspiration_hits.sdf" \
+	--input $(pwd)/$KEY"_syndirella_input.csv" \
+	--hits_path $(pwd)/"a71ev2a_c01_elabs_syndirella_inspiration_hits.sdf" \
 	--output $(pwd)/$KEY \
-	--metadata "/opt/xchem-fragalysis-2/maxwin/BulkDock/TARGETS/A71EV2A/metadata.csv" \
 	--templates "$(pwd)/templates" \
+    --scaffold_place_num 20 \
     --db_search_tool hippo \
     --reference_db "/opt/xchem-fragalysis-2/maxwin/EnamineCatalogs/enamine_bb_hippo.sqlite" \
-	--no_scaffold_place \
-    --use_sdf_names \
-    --manual
+    --manual \
+	--no_scaffold_place
+#    --no_assert_scaffold_intra_geom_flatness
     
 # sb.sh --job-name syndirella $HOME2/slurm/run_bash_with_conda.sh ./run_elaboration.sh
 
